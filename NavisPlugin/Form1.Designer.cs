823a3a0b18510051a1c78de39863acc02304a36b
Guilherme Araujo (MR) <guilherme@autodoc.com.br>
2020-12-16 11:40:28 -0200

    Add project files.

---
+++ NavisPlugin/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace NavisPlugin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btStart = new System.Windows.Forms.Button();
+            this.btAppendModel = new System.Windows.Forms.Button();
+            this.btClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(13, 13);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(169, 23);
+            this.btStart.TabIndex = 0;
+            this.btStart.Text = "StartNavisworks";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btStart_mouseUp);
+            // 
+            // btAppendModel
+            // 
+            this.btAppendModel.Location = new System.Drawing.Point(12, 42);
+            this.btAppendModel.Name = "btAppendModel";
+            this.btAppendModel.Size = new System.Drawing.Size(170, 23);
+            this.btAppendModel.TabIndex = 1;
+            this.btAppendModel.Text = "AppendModel";
+            this.btAppendModel.UseVisualStyleBackColor = true;
+            this.btAppendModel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btAppendModel_mouseUp);
+            // 
+            // btClose
+            // 
+            this.btClose.Location = new System.Drawing.Point(12, 71);
+            this.btClose.Name = "btClose";
+            this.btClose.Size = new System.Drawing.Size(170, 23);
+            this.btClose.TabIndex = 2;
+            this.btClose.Text = "Close";
+            this.btClose.UseVisualStyleBackColor = true;
+            this.btClose.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btClose_mouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(204, 105);
+            this.Controls.Add(this.btClose);
+            this.Controls.Add(this.btAppendModel);
+            this.Controls.Add(this.btStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Button btAppendModel;
+        private System.Windows.Forms.Button btClose;
+    }
+}
+
---
+++ NavisPlugin/Form1.cs
@@ -0,0 +1,86 @@
+﻿using Autodesk.Navisworks.Api.Automation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NavisPlugin
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Navisworks App
+        /// </summary>
+        public NavisworksApplication NwApp { get; set; }
+        
+        /// <summary>
+        /// Construtor
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Inicia o Navisworks
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btStart_mouseUp(object sender, MouseEventArgs e)
+        {
+            //Inicia o Navisworks
+            NwApp = new NavisworksApplication();
+            //Define a janela do Navisworks está visivel.
+            NwApp.Visible = true;
+        }
+
+        /// <summary>
+        /// Anexa modelos ao Navisworks
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btAppendModel_mouseUp(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                var dg = new OpenFileDialog();
+                if (dg.ShowDialog()==DialogResult.OK)
+                {
+                    //Append do modelo
+                    NwApp.AppendFile(dg.FileName);
+                }
+            }
+            catch (Exception exception)
+            {
+                //Messagem de erro
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            //Fechando o Navisworks
+            NwApp.Dispose();
+            base.OnClosing(e);
+        }
+
+        /// <summary>
+        /// Fecha o Navisworks
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btClose_mouseUp(object sender, MouseEventArgs e)
+        {
+            NwApp.Dispose();
+        }
+    }
+}
