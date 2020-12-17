8a17753419e55d8337b4fdd7400bb854d68aeebf
Guilherme Araujo (MR) <guilherme@autodoc.com.br>
2020-12-17 10:07:17 -0200

    DEV-7679 aula 04 navisworks

---
+++ Aula03/Ctr/UcUpdate.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace Aula03.Ctr
+{
+    partial class UcUpdate
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btUpdate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btUpdate
+            // 
+            this.btUpdate.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btUpdate.Location = new System.Drawing.Point(0, 0);
+            this.btUpdate.Name = "btUpdate";
+            this.btUpdate.Size = new System.Drawing.Size(306, 23);
+            this.btUpdate.TabIndex = 0;
+            this.btUpdate.Text = "Update";
+            this.btUpdate.UseVisualStyleBackColor = true;
+            this.btUpdate.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btUpdate_MouseUp);
+            // 
+            // UcUpdate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btUpdate);
+            this.Name = "UcUpdate";
+            this.Size = new System.Drawing.Size(306, 446);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btUpdate;
+    }
+}
---
+++ Aula03/Ctr/UcUpdate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula03.Ctr
+{
+    public partial class UcUpdate : UserControl
+    {
+        public UcUpdate()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnParentChanged(EventArgs e)
+        {
+            base.OnParentChanged(e);
+            Dock = DockStyle.Fill;
+        }
+
+        private void btUpdate_MouseUp(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("Update");
+        }
+    }
+}
